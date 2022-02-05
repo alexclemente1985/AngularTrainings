8867a1f8b58acd13a71518061434da65d661e5af
Alexandre Pinheiro <alexclemente1985@gmail.com>
2022-02-05 10:44:27 -0300

    Ajustes

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -2,7 +2,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 const router = express.Router();
 const Product = require('../models/Product')
-console.log(require('dotenv').config())
+require('dotenv').config();
 
 try{
     mongoose.connect(
