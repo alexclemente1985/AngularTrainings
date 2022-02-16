a36c3f2ff75a842d784af71f1a7c71d73b35803e
Alexandre C. Pinheiro <alexclemente1985@gmail.com>
2022-02-15 21:44:07 -0300

    Aprimorando o componente de lista de categorias

--- financys/src/app/pages/categories/category-list/category-list.component.ts
+++ financys/src/app/pages/categories/category-list/category-list.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { catchError, filter, switchMap, throwError } from 'rxjs';
+import { Category } from '../shared/models/category.model';
+import { CategoryService } from '../shared/services/category.service';
 
 @Component({
   selector: 'app-category-list',
@@ -7,13 +10,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class CategoryListComponent implements OnInit {
 
-  constructor() { }
+  categories: Category[] = [];
+
+  constructor(
+    private categoryService: CategoryService
+  ) { }
 
   ngOnInit(): void {
+    this.categoryService.getAll()
+    .subscribe({
+      next: (categories) => this.categories = categories,
+      error: () => alert("Erro ao carregar a lista")
+    })
   }
 
-  alert(){
-    alert('excluir');
+  deleteCategory(category: Category){
+    const mustDelete = confirm("Deseja realmente excluir este item?");
+    
+    if(mustDelete){
+      console.log('caiu no delete')
+      this.categoryService.delete(category.id as number)
+      .subscribe({
+        next: () => this.categories = this.categories.filter(element => element != category),
+        error: (e) => alert("Erro ao tentar excluir")
+      })
+    }
+    
+    
+
   }
 
 }
