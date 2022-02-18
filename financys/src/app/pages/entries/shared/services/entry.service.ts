88685e87d4865f1183521b75d75e4c4ceea87466
Alexandre C. Pinheiro <alexclemente1985@gmail.com>
2022-02-18 16:16:59 -0300

    Melhorando EntryService

--- financys/src/app/pages/entries/shared/services/entry.service.ts
+++ financys/src/app/pages/entries/shared/services/entry.service.ts
@@ -2,7 +2,7 @@ import { Injector, Injectable } from '@angular/core';
 import { CategoryService } from 'src/app/pages/categories/shared/services/category.service';
 import { Entry } from '../models/entry.model';
 import { BaseResourceService } from 'src/app/shared/services/base-resource/base-resource.service';
-import { mergeMap, Observable } from 'rxjs';
+import { mergeMap, Observable, ObservableInput, catchError } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
@@ -13,28 +13,25 @@ export class EntryService extends BaseResourceService<Entry>{
     protected override injector: Injector,
     private categoryService: CategoryService
   ) {
-    super("api/entries",injector, Entry.fromJson)
+    super("api/entries", injector, Entry.fromJson)
    }
 
   override create(entry: Entry): Observable<Entry>{
-   return this.categoryService.getById(entry.categoryId as number)
-    .pipe(
-      //permite o retorno de Observable<Entry> e não Observable<Observable<Entry>>)
-      mergeMap(category => {
-        entry.category = category;
-        // Reaproveita BaseResourceService no método homônimo
-        return super.create(entry);
-      })
-    )
+    return this.setCategoryAndSendToServer(entry, super.create.bind(this));
   }
 
   override update(entry: Entry): Observable<Entry>{
+    return this.setCategoryAndSendToServer(entry, super.update.bind(this));
+   }
+
+  private setCategoryAndSendToServer(entry: Entry, sendFn: any): Observable<Entry>{
     return this.categoryService.getById(entry.categoryId as number)
     .pipe(
-      mergeMap(category =>{
+      mergeMap((category): ObservableInput<any> =>{
         entry.category = category;
-        return super.update(entry);
-      })
+        return sendFn(entry);
+      }),
+      catchError(this.handleError)
     )
   }
 }
