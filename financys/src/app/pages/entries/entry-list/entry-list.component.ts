4195f905df58bd6f7143ff08e44b95e034de59b0
Alexandre C. Pinheiro <alexclemente1985@gmail.com>
2022-02-19 06:50:21 -0300

    Melhoria do EntryList com BaseResourceList

--- financys/src/app/pages/entries/entry-list/entry-list.component.ts
+++ financys/src/app/pages/entries/entry-list/entry-list.component.ts
@@ -1,3 +1,4 @@
+import { BaseResourceListComponent } from 'src/app/shared/components/base-resource-list/base-resource-list.component';
 import { Component, OnInit } from '@angular/core';
 import { Entry } from '../shared/models/entry.model';
 import { EntryService } from '../shared/services/entry.service';
@@ -7,35 +8,11 @@ import { EntryService } from '../shared/services/entry.service';
   templateUrl: './entry-list.component.html',
   styleUrls: ['./entry-list.component.scss']
 })
-export class EntryListComponent implements OnInit {
-
-  entries: Entry[] = [];
+export class EntryListComponent extends BaseResourceListComponent<Entry> {
 
   constructor(
     private entryService: EntryService
-  ) { }
-
-  ngOnInit(): void {
-    this.entryService.getAll()
-    .subscribe({
-      next: (entries) => this.entries = entries.sort((a,b)=> (b.id as number) - (a.id as number)),
-      error: () => alert("Erro ao carregar a lista")
-    })
-  }
-
-  deleteEntry(entry: Entry){
-    const mustDelete = confirm("Deseja realmente excluir este item?");
-
-    if(mustDelete){
-      this.entryService.delete(entry.id as number)
-      .subscribe({
-        next: () => this.entries = this.entries.filter(element => element != entry),
-        error: (e) => alert("Erro ao tentar excluir")
-      })
-    }
-
-
-
-  }
-
+  ) {
+    super(entryService)
+   }
 }
