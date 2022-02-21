7923a7de3c01d3e22c071d3f0302c84058c90d08
Alexandre C. Pinheiro <alexclemente1985@gmail.com>
2022-02-21 01:04:08 -0300

    Preparação para gráficos e ajustes tsconfig.json

--- financys/src/app/pages/entries/shared/services/entry.service.ts
+++ financys/src/app/pages/entries/shared/services/entry.service.ts
@@ -44,8 +44,7 @@ export class EntryService extends BaseResourceService<Entry>{
   }
 
   private filterByMonthAndYear(entries: Entry[], month: number, year: number){
-    return entries.filter(
-      entry => {
+    return entries.filter(entry => {
         const entryDate = moment(entry.date, "DD/MM/YYYY");
         const monthMatches = entryDate.month() + 1 == month;
         const yearMatches = entryDate.year() == year;
--- financys/src/app/pages/reports/reports/reports.component.ts
+++ financys/src/app/pages/reports/reports/reports.component.ts
@@ -1,4 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as currencyFormater from 'currency-formatter'
+import { Category } from '../../categories/shared/models/category.model';
+import { CategoryService } from '../../categories/shared/services/category.service';
+import { Entry } from '../../entries/shared/models/entry.model';
+import { EntryService } from '../../entries/shared/services/entry.service';
 
 @Component({
   selector: 'app-reports',
@@ -7,13 +12,57 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ReportsComponent implements OnInit {
 
-  constructor() { }
+  expenseTotal: any = 0;
+  revenueTotal: any = 0;
+  balance: any = 0;
+
+  expenseChartData: any;
+  revenueChartData: any;
+
+  chartOptions = {
+    scales: {
+      yAxis: [{
+        ticks: {
+            beginAtZero: true
+          }
+      }
+      ]
+    }
+  }
+
+  categories: Category[] = [];
+  entries: Entry[] = [];
+
+
+  @ViewChild('month') month: ElementRef | null = null;
+  @ViewChild('year') year: ElementRef | null = null;
+
+  constructor(
+    private entryService: EntryService,
+    private categoryService: CategoryService
+  ) { }
 
   ngOnInit(): void {
+    this.categoryService.getAll()
+    .subscribe({
+      next: (categories => this.categories = categories)
+    }
+    )
   }
 
   generateReports(){
-    
+    const month = this.month?.nativeElement.value;
+    const year = this.year?.nativeElement.value;
+
+    if(!month || !year){
+      alert('Você precisa selecionar Mês e Ano para gerar os relatórios');
+    }
+    else {
+      this.entryService.getByMonthAndYear(month, year)
+      .subscribe({
+        next: () => {}
+      });
+    }
   }
 
 }
